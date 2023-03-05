34fc25868c2296c7ea76ba03c30ffd57fdd4849a
minsu235 <ohminsu1@g.skku.edu>
2023-03-06 03:01:39 +0900

    빌드 오류 수정 및 gitignore 수정
    1. EngineCopy.bat 에 inl 파일 복사하도록 수정
    2. ico 파일 추가 및 gitignore에서 제외 부분 삭제
    3. GITIGNORE 폴더 아래로는 ignore. ( unitybuild 파일 )
    4. idb, exe,  ini 파일 ignore 추가.
    5. unitybuild 경로 수정

--- EngineCopy.bat
+++ EngineCopy.bat
@@ -1,2 +1,3 @@
 xcopy /s /y  /exclude:exclude_list.txt ".\01. Project\Engine\*.h" ".\03. External\include\Engine"
+xcopy /s /y  /exclude:exclude_list.txt ".\01. Project\Engine\*.inl" ".\03. External\include\Engine"
 xcopy /s /y  /exclude:exclude_list.txt ".\01. Project\Engine\*.fx" ".\02. OutPut\bin\content\shader"
